ea29ce78ab23e2926bcc7bbc7597121ee7c7b392
sennoun <ysennoun@xebia.fr>
2018-11-13 22:54:19 +0100

    Integrate count with AWS.

--- server.js
+++ server.js
@@ -32,8 +32,30 @@ var optionsServeCommands = {
         type: '__type__'
     }
 };
+var optionsGetCommandsAWS = {
+    url: "https://gwhzffook7.execute-api.eu-west-1.amazonaws.com/Stage/command",
+    json: true,
+    method: "GET"
+};
 app.get('/', function(req, res) {
-    res.redirect('/commands');
+    res.redirect('/summary');
+});
+app.get('/summary', function(req, res) {
+    request(optionsGetCommandsAWS, function (error, response, body) {
+          if(error == null && response!= null && response.statusCode == 200) {
+            var countAll = Object.keys(body).length;
+            var countBeer = 0;
+            var countFood = 0;
+            Object.keys(body).forEach(function(value){
+              if (body[value].beer != null) countBeer++;
+              if (body[value].food != null) countFood++;
+            });
+            res.render('summary.ejs', {data: [countAll, countBeer, countFood]});
+          }else {
+            res.status(500).send({error: 'Error after request Azure.'});
+          }
+        });
+
 });
 app.get('/commands', function(req, res) {
     request(optionsGetCommands, function (error, response, body) {
